4a4b54488229b45b8d09de4a808a7077c8e631a0
Engler Gonzalez <englergonzalez@hotmail.com>
2020-11-26 20:27:57 -0500

    foto

--- resources/views/catalogo/perros.blade.php
+++ resources/views/catalogo/perros.blade.php
@@ -19,48 +19,12 @@
 
             <div class="col-lg-4 col-md-6 portfolio-item">
               <div class="portfolio-wrap">
-               @if( $c['id'] == 101 )
-               <img src="{{url( 'img/productos/producto-1.jpg')}}" class="img-fluid" alt="">
+               <img src="{{ url( 'img/productos/'.$c['foto']) }}" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4>{{ $c['nombre']}}</h4>
                   <p>{{ $c['precio']}}</p>
                   <div class="portfolio-links">
-                    <a href="{{url('img/productos/producto-1.jpg')}}" data-gall="portfolioGallery" class="venobox"><i class="bx bx-plus"></i>Imagen</a> |@endif
-                    @if( $c['id'] == 102 )
-               <img src="{{url( 'img/productos/producto-2.jpg')}}" class="img-fluid" alt="">
-                <div class="portfolio-info">
-                  <h4>{{ $c['nombre']}}</h4>
-                  <p>{{ $c['precio']}}</p>
-                  <div class="portfolio-links">
-                    <a href="{{url('img/productos/producto-2.jpg')}}" data-gall="portfolioGallery" class="venobox"><i class="bx bx-plus"></i>Imagen</a> |@endif
-                    @if( $c['id'] == 103 )
-               <img src="{{url( 'img/productos/producto-3.jpg')}}" class="img-fluid" alt="">
-                <div class="portfolio-info">
-                  <h4>{{ $c['nombre']}}</h4>
-                  <p>{{ $c['precio']}}</p>
-                  <div class="portfolio-links">
-                    <a href="{{url('img/productos/producto-3.jpg')}}" data-gall="portfolioGallery" class="venobox"><i class="bx bx-plus"></i>Imagen</a> |@endif
-                    @if( $c['id'] == 104 )
-               <img src="{{url( 'img/productos/producto-4.jpg')}}" class="img-fluid" alt="">
-                <div class="portfolio-info">
-                  <h4>{{ $c['nombre']}}</h4>
-                  <p>{{ $c['precio']}}</p>
-                  <div class="portfolio-links">
-                    <a href="{{url('img/productos/producto-4.jpg')}}" data-gall="portfolioGallery" class="venobox"><i class="bx bx-plus"></i>Imagen</a> |@endif
-                    @if( $c['id'] == 105 )
-               <img src="{{url( 'img/productos/producto-5.jpg')}}" class="img-fluid" alt="">
-                <div class="portfolio-info">
-                  <h4>{{ $c['nombre']}}</h4>
-                  <p>{{ $c['precio']}}</p>
-                  <div class="portfolio-links">
-                    <a href="{{url('img/productos/producto-5.jpg')}}" data-gall="portfolioGallery" class="venobox"><i class="bx bx-plus"></i>Imagen</a> |@endif
-                    @if( $c['id'] == 106 )
-               <img src="{{url( 'img/productos/producto-6.jpg')}}" class="img-fluid" alt="">
-                <div class="portfolio-info">
-                  <h4>{{ $c['nombre']}}</h4>
-                  <p>{{ $c['precio']}}</p>
-                  <div class="portfolio-links">
-                    <a href="{{url('img/productos/producto-6.jpg')}}" data-gall="portfolioGallery" class="venobox"><i class="bx bx-plus"></i>Imagen</a> |@endif
+                    <a href="{{ url( 'img/productos/'.$c['foto']) }}" data-gall="portfolioGallery" class="venobox"><i class="bx bx-plus"></i>Imagen</a> |
                     <a href='{{ url("catalogo/detalle/$c[id]/$c[nombre]/$c[precio]/$c[foto]") }}'> 
                     Comprar </a>
                   </div>
